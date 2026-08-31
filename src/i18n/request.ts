34d20aa595e1cb0dabfd7f41e2c5ef9f057f256f
Nam <nam.villari@vlc2.com>
2026-08-31 12:02:01 +0200

    fix segnalazione 9

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,10 +1,11 @@
 import { getRequestConfig } from 'next-intl/server';
+import itMessages from '../messages/it.json';
 
 export default getRequestConfig(async () => {
   const locale = 'it';
 
   return {
     locale,
-    messages: (await import('../messages/it.json')).default,
+    messages: itMessages,
   };
 });
